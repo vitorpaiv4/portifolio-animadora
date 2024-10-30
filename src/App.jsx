6e6a0caa0540335d1e33f591cb96bca0b593226f
vitor paiva <paivavitorr@gmail.com>
2024-10-30 15:24:22 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-red-500'><h1>Hello World</h1></div>
+  )
+}
+
+export default App
