59d3fe13842340cc7b89843ea5a90ae685f227b9
vitor paiva <paivavitorr@gmail.com>
2024-10-30 18:04:32 -0400

    sla

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,12 +5,12 @@ import { faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 const Layout = () => {
   return (
     <div 
-      className="fixed top-0 left-0 w-full bg-pink-200 py-4 z-50" // Barra fixa com fundo rosa
-      style={{ boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)' }}  // Sombra suave na barra
+      className="fixed top-0 left-0 w-full bg-pink-300 py-4 z-50"  // Barra fixa com fundo rosa
+      style={{ boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)' }}     // Sombra suave na barra
     >
-      <div className="container mx-auto flex justify-between items-center px-4">
-        {/* Links de navegação */}
-        <div className="flex space-x-8">
+      <div className="container mx-auto flex justify-center items-center px-4">
+        {/* Links de navegação e ícones centralizados */}
+        <div className="flex items-center space-x-8">
           <a href="#about" className="text-white text-lg font-bold hover:text-light-pink" 
              style={{ textShadow: '1px 1px 3px rgba(0, 0, 0, 0.6)' }}>
             Sobre
@@ -23,10 +23,7 @@ const Layout = () => {
              style={{ textShadow: '1px 1px 3px rgba(0, 0, 0, 0.6)' }}>
             Contatos
           </a>
-        </div>
-
-        {/* Ícones de Redes Sociais */}
-        <div className="flex items-center space-x-4">
+          {/* Ícones de Redes Sociais */}
           <a href="https://www.linkedin.com/" className="text-white text-2xl hover:text-light-pink">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
